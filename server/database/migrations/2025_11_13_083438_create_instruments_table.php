85a1bb2448374c43c559a7290e9e9fa5add1bd58
bmate07 <b.mate.j07@gmail.com>
2025-11-13 12:07:06 +0100

    database

--- server/database/migrations/2025_11_13_083438_create_instruments_table.php
+++ server/database/migrations/2025_11_13_083438_create_instruments_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,7 +19,6 @@ return new class extends Migration {
             $table->integer('quantity')->nullable()->default(null);
         });
     }
-
 
     /**
      * Reverse the migrations.
